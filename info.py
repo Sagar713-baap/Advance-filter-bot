7373e32596745d1aec6575b5ea3b4e9b39c8b323
Sagar713-baap <videoasmr63@gmail.com>
2024-07-21 13:50:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ def is_enabled(value, default):
         return default
 
 # Bot information
-SESSION = environ.get('SESSION', 'TechVJBot')
+SESSION = environ.get('SESSION', 'Moviesearchbot')
 API_ID = int(environ.get('API_ID', '14296467'))
 API_HASH = environ.get('API_HASH', 'a61d6246cb937abc1640aee0ec41fba3')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
